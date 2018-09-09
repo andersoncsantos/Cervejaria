f79a12786ca8edf6e5f03269d537d0f696530e70
andersoncsantos <andersonc1981@gmail.com>
2018-09-09 00:39:06 -0300

    09-09-2018

--- src/main/java/com/anderson/brewer/repository/helper/usuario/UsuariosQueries.java
+++ src/main/java/com/anderson/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.anderson.brewer.model.Usuario;
+import com.anderson.brewer.repository.filter.UsuarioFilter;
 
 public interface UsuariosQueries {
 
 	public Optional<Usuario> porEmailEAtivo(String email);
-	
 	public List<String> permissoes(Usuario usuario);
+	public List<Usuario> filtrar(UsuarioFilter filtro);
 }
 
 
